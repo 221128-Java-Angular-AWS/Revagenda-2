d710944796a3c7dc6eb8ed39caa9c31631c00dbe
LiquidPlummer <88201685+LiquidPlummer@users.noreply.github.com>
2023-01-03 15:17:46 -0600

    Update DemoApplicationTests.java

--- src/test/java/com/revagenda/DemoApplicationTests.java
+++ src/test/java/com/revagenda/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.revagenda;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
 
 @SpringBootTest
 class DemoApplicationTests {
+	private int i;
 
 	@Test
 	void contextLoads() {
+		System.out.println("This is a test. Let's cause a merge conflict.");
+	}
+	
+	void testMethod() {
+		//this is a test
+		System.out.println("Lorem Ipsum stuff");
 	}
 
 }
