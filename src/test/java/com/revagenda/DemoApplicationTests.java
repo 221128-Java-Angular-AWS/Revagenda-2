0fc0411b0e5aaafc1379b435dbac5561052ffbf3
Kyle <Kyle.Plummer@revature.com>
2023-01-03 15:26:25 -0600

    made changes

--- src/test/java/com/revagenda/DemoApplicationTests.java
+++ src/test/java/com/revagenda/DemoApplicationTests.java
@@ -6,15 +6,22 @@ import org.junit.jupiter.api.Test;
 @SpringBootTest
 class DemoApplicationTests {
 	private int i;
+	private Integer test;
 
 	@Test
 	void contextLoads() {
 		System.out.println("This is a test. Let's cause a merge conflict.");
+		this.test = 5;
 	}
 	
 	void testMethod() {
 		//this is a test
 		System.out.println("Lorem Ipsum stuff");
+	}
+
+	void testTwo() {
+		//test
+		System.out.println("This might break it...");
 	}
 
 }
